b00aa24f1723bec6a92c8c3060958b1a2ff29f4e
rampage128 <frederik.wolter@googlemail.com>
2017-10-24 23:01:59 +0200

    Add remote control id to packet handling

--- app/src/main/java/de/jlab/cardroid/car/RemoteControl.java
+++ app/src/main/java/de/jlab/cardroid/car/RemoteControl.java
@@ -3,13 +3,18 @@ package de.jlab.cardroid.car;
 import de.jlab.cardroid.usb.CarSystemSerialPacket;
 
 public class RemoteControl extends CarSystem {
+    private int controlId;
     private int buttonId;
 
     @Override
     protected void updateDataFromPacket(CarSystemSerialPacket packet) {
+        this.controlId = packet.readUnsignedByte(0);
         this.buttonId = packet.readUnsignedByte(0);
     }
 
+    public int getControlId() {
+        return this.controlId;
+    }
     public int getButtonId() {
         return this.buttonId;
     }
